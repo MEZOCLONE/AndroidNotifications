c65385f76c1c1d59495c462e5b4e7e8b3c083e54
Matt Mackay <mattem@gmail.com>
2013-04-29 22:58:07 +0100

    TestSanity

--- RemoteNotifier-Test/src/com/matt/remotenotifier/test/SanityCheckTest.java
+++ RemoteNotifier-Test/src/com/matt/remotenotifier/test/SanityCheckTest.java
@@ -4,9 +4,9 @@ import com.matt.remotenotifier.MainFragmentActivity;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-public class SanityCheck extends ActivityInstrumentationTestCase2<MainFragmentActivity> {
+public class SanityCheckTest extends ActivityInstrumentationTestCase2<MainFragmentActivity> {
 
-	public SanityCheck(Class<MainFragmentActivity> activityClass) {
+	public SanityCheckTest(Class<MainFragmentActivity> activityClass) {
 		super(activityClass);
 	}
 	
