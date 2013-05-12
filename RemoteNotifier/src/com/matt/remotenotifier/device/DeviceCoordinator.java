fe63a1f11f199127e7d4af4454306e0e4a3ec807
Matt Mackay <mattem@gmail.com>
2013-05-12 21:22:22 +0100

    Now responsible for add commands and arguments

--- RemoteNotifier/src/com/matt/remotenotifier/device/DeviceCoordinator.java
+++ RemoteNotifier/src/com/matt/remotenotifier/device/DeviceCoordinator.java
@@ -3,6 +3,7 @@ package com.matt.remotenotifier.device;
 import java.io.NotActiveException;
 import java.util.ArrayList;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,11 +21,14 @@ import android.util.Log;
  * Singleton instance - Use getInstance 
  */
 public class DeviceCoordinator {
-	private static final String TAG = DeviceCoordinator.class.getName();
+	private static final String TAG = DeviceCoordinator.class.getSimpleName();
 	private static DeviceCoordinator instance;
 	private Integer deviceCount;
 	private ArrayList<DeviceHolder> deviceList;
 	private Context ctx;
+	private DeviceDao deviceDao;
+	private CommandDao commandDao;
+	private ArgumentDao argumentDao;
 	private DeviceManagementTask dmt;
 	private DeviceHeartbeatTask dht;
 	private ChannelEventCoordinator cec;
@@ -51,6 +55,10 @@ public class DeviceCoordinator {
 		deviceCount = 0;
 		deviceList = new ArrayList<DeviceHolder>();
 		this.ctx = ctx;
+		deviceDao = new DeviceDao(this.ctx);
+		commandDao = new CommandDao(this.ctx);
+		argumentDao = new ArgumentDao(this.ctx);
+		//restoreDeviceHolderList();
 		getChannelEventCoordinatorInstance();
 		Log.i(TAG, "Device Coodinator Started Okay");
 	}
@@ -91,14 +99,19 @@ public class DeviceCoordinator {
 		
 	}
 	
-	public void restoreDeviceHolderList(ArrayList<DeviceHolder> deviceList) throws NotActiveException{
-		if(instance != null){
-			this.deviceList = deviceList;
-			deviceCount = deviceList.size();
-			updateControl();
-		}else{
-			throw new NotActiveException("Device Coordinator not yet active");
+	private void restoreDeviceHolderList(){
+		deviceList.clear();
+		deviceList = deviceDao.getAllDevices();
+		deviceCount = deviceList.size();
+		
+		for(DeviceHolder device : deviceList){
+			device.restoreCommandList(commandDao.getAllCommands(device));
+			
+			for(CommandHolder command : device.getCommandList()){
+				command.restoreArgumentList(argumentDao.getAllArguments(command));
+			}
 		}
+		updateControl();
 	}
 	
 	public boolean deviceHolderExists(String deviceName, DeviceType deviceType){
@@ -114,7 +127,7 @@ public class DeviceCoordinator {
 		DeviceHolder device = new DeviceHolder(deviceName, deviceType);
 		if(!deviceList.contains(device)){
 			Log.i(TAG, "Registering device ["+device.getDeviceName()+"]");
-			deviceList.add(device);
+			deviceList.add(deviceDao.createDevice(device));
 			// We can't control controllers, that would be insane.
 			deviceCount++;
 			updateControl();
@@ -129,7 +142,7 @@ public class DeviceCoordinator {
 	public int registerDevice(DeviceHolder device){
 		if(!deviceList.contains(device)){
 			Log.i(TAG, "Registering device ["+device.getDeviceName()+"]");
-			deviceList.add(device);
+			deviceList.add(deviceDao.createDevice(device));
 			// We can't control controllers, that would be insane.
 			deviceCount++;
 			updateControl();
@@ -141,14 +154,55 @@ public class DeviceCoordinator {
 		}
 	}
 	
-	public void addCommandsToDevice(DeviceHolder device, JSONObject commandList) throws JSONException{
+	public void configureDevice(DeviceHolder device, JSONObject config) throws JSONException {
+		// Do this so we make sure we are working with the one in the list, not the instance passed in
+		device = getDeviceHolder(device.getDeviceName(), device.getDeviceType());
+		
+		addCommandsToDevice(device, config);
+	}
+	
+	protected void addCommandsToDevice(DeviceHolder device, JSONObject commandList) throws JSONException{	
 		Log.i(TAG, "Adding commands to device");
-		device.addCommand(commandList);
+		JSONArray commandArray = commandList.getJSONArray("commands");
+		for(int i=0; i < commandArray.length(); ++i){
+			JSONObject j = commandArray.getJSONObject(i);
+			CommandHolder command = commandDao.createCommand(device, j.getString("name"),j.getString("com"));
+			addArgumentsToCommand(command, j);
+			device.addCommand(command);
+		}
 		updateControl();
 	}
 	
+	protected void addArgumentsToCommand(CommandHolder command, JSONObject argumentList) throws JSONException{
+		JSONArray argArray;
+		try{
+			argArray = argumentList.getJSONArray("args");
+		}catch(JSONException e){
+			Log.w(TAG, "No arguments found for command ["+argumentList.getString("name")+"]");
+			return;
+		}
+		Log.d(TAG, "Arguments found for command ["+argumentList.getString("name")+"]");
+		for(int i=0; i < argArray.length(); ++i){
+			JSONObject j = argArray.getJSONObject(i);
+			Log.d(TAG, "Adding ["+j.getString("name")+"] with type ["+j.getString("type")+"]");
+			ArgumentHolder arg = argumentDao.createArgument(command, j.getString("name"),j.getString("type"));
+			command.addArgument(arg);
+		}
+	}
+	
 	public void addCommandsToDevice(DeviceHolder device, String name, String command) throws JSONException{
-		device.addCommand(name, command);
+		CommandHolder commandHolder = commandDao.createCommand(device, name, command);
+		device.addCommand(commandHolder);
+	}
+	
+	public void addArgumentsToCommand(CommandHolder command, String name, String type){
+		ArgumentHolder argument = new ArgumentHolder(name, type);
+		command.addArgument(argument);
+	}
+	
+	public void setArgumentValue(DeviceHolder device, CommandHolder command, int argumentId, String argumentValue){
+		command.setArgumentValue(argumentId, argumentValue);
+		argumentDao.setValue(command.getArgument(argumentId), argumentValue);
 	}
 	
 	public int getDeviceCount(){
@@ -184,6 +238,7 @@ public class DeviceCoordinator {
 	public void deregisterDevice(DeviceHolder device){
 		Log.i(TAG, "Deregistering device ["+device.getDeviceName()+"]");
 		deviceList.remove(device);
+		deviceDao.destroyDevice(device);
 		deviceCount--;
 		updateControl();
 	}
@@ -191,6 +246,7 @@ public class DeviceCoordinator {
 	public void deregisterDevice(int index){
 		Log.i(TAG, "Deregistering device ["+getDeviceHolder(index).getDeviceName()+"]");
 		deviceList.remove(index);
+		deviceDao.destroyDevice(getDeviceHolder(index));
 		deviceCount--;
 		updateControl();
 	}
@@ -205,7 +261,7 @@ public class DeviceCoordinator {
 				try {
 					pam = PagerAdapterManager.getInstance();
 					CommandFragment commandFragment = (CommandFragment) pam.getItem(1);
-					commandFragment.mAdaptor.notifyDataSetChanged();
+					commandFragment.refreshCommandFragment();
 				} catch (NotActiveException e) {
 					Log.w(TAG, "Error getting PagerAdapterManager");
 				}				
@@ -214,17 +270,18 @@ public class DeviceCoordinator {
 	}
 	
 	public void requestHeartbeatFromDevice(final String deviceName){
+		getChannelEventCoordinatorInstance();
 		Thread requestHeatbeatThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					JSONObject jObj = new JSONObject("{requestedDevice: "+deviceName+", senderType: controller}");
+					JSONObject jObj = new JSONObject("{requestedDevice: "+deviceName+", requestedDeviceTag: "+ChannelEventCoordinator.DEVICE_TAG+", senderType: controller}");
 					Log.i(TAG, "Requesting heatbeat from "+deviceName);
-					cec.trigger(0, "client-heartbeat_request", jObj.toString());
+					cec.trigger(0, ChannelEventCoordinator.EVENT_HEARTBEAT_REQUEST, jObj.toString());
 				} catch (JSONException e) {
-					Log.w(TAG, "Error requesting heartbeat from device "+deviceName, e);
+					Log.w(TAG, "Error creating Json Object", e);
 				} catch (Exception e) {
-					Log.e(TAG, e.getMessage());
+					Log.e(TAG, "Error requesting heartbeat from device "+deviceName, e);
 				}
 			}
 		});
@@ -237,6 +294,7 @@ public class DeviceCoordinator {
 			throw new Exception("Unable to handle heartbeat - Device ["+deviceName+"] does not exist");
 		}else{
 			dh.touchHeartbeatTime();
+			deviceDao.updateHeartbeatTime(dh);
 			Log.i(TAG, "Heartbeat revieved okay from ["+deviceName+"]");
 		}
 	}
